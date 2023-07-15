35ac662c7885971d931c3a404b5080fb98aab749
michellqueiroz <michellfelippe@dcc.ufba.br>
2023-07-14 21:19:46 -0400

    small changes

--- REQreate/REQreate3.py
+++ REQreate/REQreate3.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
 
     #EDIT HERE
     place_name = "Chicago, Illinois"
-    inst_directory = '../examples/ODBRP_benchmark_configuration_files/300_1000_3000/'
+    inst_directory = '../examples/ODBRP_benchmark_configuration_files/900_1000_3000/'
     #inst_directory = '../examples/testes/'
-    base_save_folder_name = '300_1000_3000' #give a unique folder name to save the instances
+    base_save_folder_name = '900_1000_3000' #give a unique folder name to save the instances
     #EDIT HERE
 
     directory = os.fsencode(inst_directory)
