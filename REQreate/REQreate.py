15e044f6ed58485564ceedbec942dc8411cdbeb3
michellqueiroz <michellfelippe@dcc.ufba.br>
2023-07-25 17:28:24 +0200

    exp files up22

--- REQreate/REQreate.py
+++ REQreate/REQreate.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
 
     #EDIT HERE
     place_name = "Chicago, Illinois"
-    inst_directory = '../examples/ODBRP_benchmark_configuration_files/1500_180_1000/'
+    inst_directory = '../examples/ODBRP_benchmark_configuration_files/2400_180_1000/'
     #inst_directory = '../examples/testes/'
-    base_save_folder_name = '1500_180_1000' #give a unique folder name to save the instances
+    base_save_folder_name = '2400_180_1000' #give a unique folder name to save the instances
     #EDIT HERE
 
     directory = os.fsencode(inst_directory)
