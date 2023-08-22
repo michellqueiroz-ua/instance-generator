1b1c4250285216f2eb9e305445c2fb20b211698f
michellqueiroz <michellfelippe@dcc.ufba.br>
2023-08-22 17:20:44 +0200

    suttm

--- REQreate/REQreate.py
+++ REQreate/REQreate.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
 
     #EDIT HERE
     place_name = "Chicago, Illinois"
-    inst_directory = '../examples/ODBRP_benchmark_configuration_files/2700_180_1000/'
+    inst_directory = '../examples/ODBRP_benchmark_configuration_files/04_example.json/'
     #inst_directory = '../examples/testes/'
-    base_save_folder_name = '2700_180_1000' #give a unique folder name to save the instances
+    base_save_folder_name = 'just_ttm' #give a unique folder name to save the instances
     #EDIT HERE
 
     directory = os.fsencode(inst_directory)
