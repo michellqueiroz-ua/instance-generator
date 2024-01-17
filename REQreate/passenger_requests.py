b9ea077026d4c74ce6d89a1724ea95afca681049
michellqueiroz <michellfelippe@dcc.ufba.br>
2024-01-17 16:51:24 +0100

    vamo ver se vai

--- REQreate/passenger_requests.py
+++ REQreate/passenger_requests.py
@@ -337,6 +337,7 @@ def _generate_single_data(GA, network, sorted_attributes, parameters, reqid, met
                                                 point = (network.schools.loc[idxs, 'lat'], network.schools.loc[idxs, 'lon'])
                                             elif parameters[loc]['locs'] == 'random':
                                                 idxs = random.choice(parameters[loc]['list'+str(replicate_num)])
+                                                print("heerexx ", idxs)
                                                 point = (parameters[loc]['list_lat'+str(replicate_num)][idxs[0]], parameters[loc]['list_lon'+str(replicate_num)][idxs[0]])
                                                 print("heere ", point)
 
